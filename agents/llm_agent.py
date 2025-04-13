df5213f860fcca0aae022ae10ae566b6ef2d5b4e
Ashwin Benke <148850788+Ashwinssj@users.noreply.github.com>
2025-04-13 17:39:40 +0530

    Update llm_agent.py

--- agents/llm_agent.py
+++ agents/llm_agent.py
@@ -1,9 +1,15 @@
-import subprocess
+import requests
 import json
-
+OLLAMA_URL="https://1315-2401-4900-1b99-ac-446-81b3-303e-bc7f.ngrok-free.app"
 
 
 def query_ollama(prompt):
-    command = ["ollama", "run", "tinyllama", prompt]
-    result = subprocess.run(command, capture_output=True, text=True)
-    return result.stdout.strip()
+    payload={
+        "model":"llama3.2",
+        "prompt":prompt
+    }
+
+    response = requests.post(OLLAMA_URL, json=payload)
+    if response.ok:
+        return response.json().get("response", "No response.")
+    return f"Ollama error: {response.status_code}

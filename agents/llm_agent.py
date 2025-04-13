e99337f42922d54181f9d6cf1002700cb3d300a6
Kunal Bagad <kunalbagad2004@gmail.com>
2025-04-13 17:51:56 +0530

    Update llm_agent.py

--- agents/llm_agent.py
+++ agents/llm_agent.py
@@ -1,15 +1,14 @@
 import requests
-import json
-OLLAMA_URL="https://1315-2401-4900-1b99-ac-446-81b3-303e-bc7f.ngrok-free.app"
 
+OLLAMA_URL = "https://1315-2401-4900-1b99-ac-446-81b3-303e-bc7f.ngrok-free.app"  # replace with your real ngrok URL
 
 def query_ollama(prompt):
-    payload={
-        "model":"llama3.2",
-        "prompt":prompt
+    payload = {
+        "model": "llama3",
+        "prompt": prompt
     }
-
     response = requests.post(OLLAMA_URL, json=payload)
     if response.ok:
         return response.json().get("response", "No response.")
-    return f"Ollama error: {response.status_code}
+    return f"Ollama error: {response.status_code}"
+

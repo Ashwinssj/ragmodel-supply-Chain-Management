f3b03b367f0020ddb022fac02ed91863952ea38b
Ashwin Benke <148850788+Ashwinssj@users.noreply.github.com>
2025-04-13 17:14:13 +0530

    Update knowledge_base.py

--- agents/knowledge_base.py
+++ agents/knowledge_base.py
@@ -1,12 +1,10 @@
 import requests
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
+TAVILY_API_KEY=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
 
 def fetch_web_insight(query):
     url = "https://api.tavily.com/search"
-    headers = {"Authorization": f"Bearer {os.getenv('TAVILY_API_KEY')}"}
+    headers = {"Authorization": f"Bearer {'TAVILY_API_KEY'}}
     payload = {
         "query": query,
         "search_depth": "advanced",
